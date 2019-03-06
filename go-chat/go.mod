49c28c3643f21733c4054e959abf3c906abfebb9
LAKuEN <y.s.k.brave@gmail.com>
2019-03-06 21:46:23 +0900

    :feat 2章 認証機能の追加 に着手

--- go-chat/go.mod
+++ go-chat/go.mod
@@ -2,4 +2,19 @@ module github.com/LAKuEN/go-programming-blueprint-ja/go-chat
 
 go 1.12
 
-require github.com/gorilla/websocket v1.4.0
+require (
+	github.com/clbanning/x2j v0.0.0-20180326210544-5e605d46809c // indirect
+	github.com/gorilla/websocket v1.4.0
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/stretchr/codecs v0.0.0-20170403063245-04a5b1e1910d // indirect
+	github.com/stretchr/gomniauth v0.0.0-20170717123514-4b6c822be2eb
+	github.com/stretchr/objx v0.1.1 // indirect
+	github.com/stretchr/signature v0.0.0-20160104132143-168b2a1e1b56 // indirect
+	github.com/stretchr/stew v0.0.0-20130812190256-80ef0842b48b // indirect
+	github.com/stretchr/testify v1.3.0 // indirect
+	github.com/stretchr/tracer v0.0.0-20140124184152-66d3696bba97 // indirect
+	github.com/ugorji/go/codec v0.0.0-20190204201341-e444a5086c43 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
+)
--- go-chat/main.go
+++ go-chat/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"path/filepath"
 	"sync"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
 )
 
 type templateHandler struct {
@@ -33,7 +38,12 @@ func main() {
 	logging := flag.Bool("logging", false, "")
 	flag.Parse()
 
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	// 認証
+	initGomniauth()
+
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 
 	r := newRoom(*logging)
 	http.Handle("/room", r)
@@ -43,4 +53,39 @@ func main() {
 	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatalf("Error occured at ListenAndServe: %#v", err)
 	}
+}
+
+func initGomniauth() {
+	// TODO 認証情報を渡す必要があるかも。以降の章進めるときに適宜直す
+	type googleClientSecret struct {
+		Web struct {
+			ClientID     string `json:"client_id"`
+			ClientSecret string `json:"client_secret"`
+		} `json:"web"`
+	}
+
+	var loadGoogleClientSecret = func() (googleClientSecret, error) {
+		jsonStr, err := ioutil.ReadFile("client_secret.apps.googleusercontent.com.json")
+		if err != nil {
+			return googleClientSecret{}, err
+		}
+		var clientSecret googleClientSecret
+		err = json.Unmarshal(jsonStr, &clientSecret)
+		if err != nil {
+			return googleClientSecret{}, err
+		}
+
+		return clientSecret, nil
+	}
+
+	clientSecret, err := loadGoogleClientSecret()
+	key, err := ioutil.ReadFile("security_key")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gomniauth.SetSecurityKey(string(key))
+	gomniauth.WithProviders(
+		google.New(clientSecret.Web.ClientID, clientSecret.Web.ClientSecret, "http://localhost:8080/auth/callback/google"),
+	)
 }
